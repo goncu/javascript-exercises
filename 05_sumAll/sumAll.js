f27e3ffde6e3fad564c05417180a33df57ff9200
goncu <116492383+goncu@users.noreply.github.com>
2022-12-18 21:37:39 +0200

    Solve

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let theSum = 0;
+  if (typeof a !== `number` || typeof b !== `number`) return `ERROR`;
+  if (a < 0 || b < 0) return `ERROR`;
+  if (b > a) {
+    for (i = a; i <= b; i++) {
+      theSum += i;
+    }
+  } else {
+    for (i = b; i <= a; i++) {
+      theSum += i;
+    }
+  }
+  return theSum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (a) {
+  const b = ((a - 32) * 5) / 9;
+  if (Number.isInteger(b)) {
+    return b;
+  } else {
+    return Number(b.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (a) {
+  const b = (a * 9) / 5 + 32;
+  if (Number.isInteger(b)) {
+    return b;
+  } else {
+    return Number(b.toFixed(1));
+  }
 };
 
+console.log(convertToCelsius(100));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
